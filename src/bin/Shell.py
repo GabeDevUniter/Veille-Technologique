7866daf65d56c446ce671d65633f043bb13467f6
GabeFlyBoy <1793245@cegepat.qc.ca>
2022-10-20 08:08:53 -0400

    Shell principal du langage

--- src/bin/Shell.py
+++ src/bin/Shell.py
@@ -1,1 +1,29 @@
-print("Hello World!")
+import sys
+from antlr4 import *
+from echoLexer import echoLexer
+from echoParser import echoParser
+from echoVisitor import echoVisitor
+
+EchoVersion = 0.1
+
+if __name__ == "__main__":
+    print(f'//------ Echo {EchoVersion} ------\\\\\n\n')
+    while 1:
+        shellInput = input('> ')
+        if shellInput[-1] != ';': shellInput += ';'
+
+        data = InputStream(shellInput)
+        # lexer
+        lexer = echoLexer(data)
+        stream = CommonTokenStream(lexer)
+        # parser
+        parser = echoParser(stream)
+        tree = parser.prog()
+        # evaluator
+        visitor = echoVisitor()
+
+        try:
+            output = visitor.visit(tree)
+            if(output != None): print(output)
+        except Exception as e:
+            print(str(e))
