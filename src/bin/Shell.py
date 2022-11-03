7359565c453a8dfdc65dba82a32cc75f9a92fe0b
GabeFlyBoy <1793245@cegepat.qc.ca>
2022-11-03 11:24:24 -0400

    Version 0.2 ajoute les comparaisons et les assignation spécials (+=,-=,++,etc.). Le Shell inclut une commande 'exit'/'quit' afin de quitter le shell sans avoir à faire ctrl+c

--- src/bin/Shell.py
+++ src/bin/Shell.py
@@ -4,12 +4,13 @@ from echoLexer import echoLexer
 from echoParser import echoParser
 from echoVisitorMain import echoVisitorMain
 
-EchoVersion = 0.1
+EchoVersion = '0.2 ALPHA'
 
 if __name__ == "__main__":
     print(f'//------ Echo {EchoVersion} ------\\\\\n\n')
     while 1:
         shellInput = input('> ')
+        if shellInput == 'exit' or shellInput == 'quit': break
         if shellInput[-1] != ';': shellInput += ';'
 
         data = InputStream(shellInput)
