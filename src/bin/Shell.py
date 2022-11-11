66dcdb93eeb82b4000570a868bf30fbc59e4c837
GabeFlyBoy <1793245@cegepat.qc.ca>
2022-11-11 00:39:07 -0500

    La commande n'ajoute pas un ; automatiquement si c'est un block if/for

--- src/bin/Shell.py
+++ src/bin/Shell.py
@@ -4,14 +4,14 @@ from echoLexer import echoLexer
 from echoParser import echoParser
 from echoVisitorMain import echoVisitorMain
 
-EchoVersion = '0.2 ALPHA'
+EchoVersion = '0.2 BETA'
 
 if __name__ == "__main__":
     print(f'//------ Echo {EchoVersion} ------\\\\\n\n')
     while 1:
         shellInput = input('> ')
         if shellInput == 'exit' or shellInput == 'quit': break
-        if shellInput[-1] != ';': shellInput += ';'
+        if shellInput[-1] != ';' and not shellInput.startswith(('if', 'elif', 'else', 'for', 'endfor', 'endif')): shellInput += ';'
 
         data = InputStream(shellInput)
         # lexer
@@ -23,7 +23,10 @@ if __name__ == "__main__":
         # evaluator
         visitor = echoVisitorMain()
 
+        # output = visitor.visit(tree)
+        # if(output != None): print(output)
         try:
+            # pass
             output = visitor.visit(tree)
             if(output != None): print(output)
         except Exception as e:
