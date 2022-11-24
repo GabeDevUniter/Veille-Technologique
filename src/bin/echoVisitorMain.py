6f3be164abdbca8ceb5c8f88ef1824b347143bc5
GabeFlyBoy <1793245@cegepat.qc.ca>
2022-11-24 01:20:28 -0500

    Ajouté la fonction ExecuteBlockInstructions pour exécuter les instructions d'un scope spécial comme IF et FOR ; Les IF/ELIF/ELSE statements sont présentement fonctionnels, mais seulement pour les blocs singuliers (Un IF à l'intérieur d'un IF ne donne pas le résultat voulu) ; Les FOR statements fonctionnent, mais le step est présentement limité à une incrémentation ; Nettoyé le code pour le rendre plus visisble

--- src/bin/echoVisitorMain.py
+++ src/bin/echoVisitorMain.py
@@ -11,6 +11,10 @@ from lib.EchoLib import *
 
 class echoVisitorMain(echoVisitor):
 
+    # +++++++++++++++++++++++++++++++++++++++
+    # ++++---- INSTRUCTION EXECUTION ----++++
+    # +++++++++++++++++++++++++++++++++++++++
+
     def ExecuteInstruction(self, ctx):
         decl = ctx.decl()
         expr = ctx.expr()
@@ -24,42 +28,14 @@ class echoVisitorMain(echoVisitor):
             return None
         return self.visit(ctx.expr()[0])
 
-    # TODO : Support nested if statements where they're only executed when the root if block ends
-    def ExecuteIfStatement(self, ifscope, ifscopeIndex):
-        hasElif = 'elif' in ifscope
-        hasElse = 'else' in ifscope
+    def ExecuteBlockInstructions(self, list):
+        for ins in list:
+            if type(ins) == dict:
+                if 'if' in ins: self.ExecuteIfStatement(ins['if'], ins['if'][2])
+                if 'for' in ins: pass
 
-        cond = self.visit(ifscope[1].expr())
-
-        if(cond):
-            # if hasElif: del ifscope['elif']
-            # elif hasElse: del ifscope['else']
-
-
-            try:
-                for ins in ifscope[0]:
-                    self.ExecuteInstruction(ins)
-            except Exception as e:
-                raise
-            finally:
-                global currentScope
-                while len(currentScope) != ifscopeIndex: PopScope()
-                print('Scope is ' + str(currentScope))
-
-        else:
-            if hasElif: self.ExecuteIfStatement(ifscope['elif'], ifscopeIndex) # Recursive
-
-            elif hasElse:
-                try:
-                    for ins in ifscope['else'][0]:
-                        self.ExecuteInstruction(ins)
-
-                except Exception as e:
-                    raise
-                finally:
-                    while len(currentScope) != ifscopeIndex: PopScope()
-                    print('Scope is ' + str(currentScope))
-
+            else:
+                self.ExecuteInstruction(ins)
 
     def visitProg(self, ctx:echoParser.ProgContext):
         # print(currentScope)
@@ -122,18 +98,64 @@ class echoVisitorMain(echoVisitor):
         elif op == '>>': return num1 >> num2
         return self.visitChildren(ctx)
 
+    # ++++++++++++++++++++++++++++++
+    # ++++---- IF STATEMENT ----++++
+    # ++++++++++++++++++++++++++++++
+
+    # TODO : Support nested if statements where they're only executed when the root if block ends
+    def ExecuteIfStatement(self, ifscope, ifscopeIndex):
+        hasElif = 'elif' in ifscope
+        hasElse = 'else' in ifscope
+
+        cond = self.visit(ifscope[1].expr())
+
+        if(cond):
+            # if hasElif: del ifscope['elif']
+            # elif hasElse: del ifscope['else']
+
+
+            try:
+                self.ExecuteBlockInstructions(ifscope[0])
+                # for ins in ifscope[0]:
+                #     self.ExecuteInstruction(ins)
+            except Exception as e:
+                raise
+            finally:
+                global currentScope
+                while len(currentScope) != ifscopeIndex: PopScope()
+                print('Scope is ' + str(currentScope))
+
+        else:
+            if hasElif: self.ExecuteIfStatement(ifscope['elif'], ifscopeIndex) # Recursive
+
+            elif hasElse:
+                try:
+                    self.ExecuteBlockInstructions(ifscope['else'][0])
+                    # for ins in ifscope['else'][0]:
+                    #     self.ExecuteInstruction(ins)
+
+                except Exception as e:
+                    raise
+                finally:
+                    while len(currentScope) != ifscopeIndex: PopScope()
+                    print('Scope is ' + str(currentScope))
+            else:
+                while len(currentScope) != ifscopeIndex: PopScope()
+
     # Visit a parse tree produced by echoParser#IfBlock.
     def visitIfBlock(self, ctx:echoParser.IfBlockContext):
         print('#IfBlock')
-        cond = self.visit(ctx.expr())
 
-        if type(cond) != bool: raise Exception('IF statement must be a condition!')
+        # if type(cond) != bool: raise Exception('IF statement must be a condition!')
+        if type(ctx.expr()).__name__ != 'CompExprContext': raise Exception('IF statement must be a condition!')
 
         AddScope('if')
 
+        # GetCurrentScope()[0] = []
         GetCurrentScope()[1] = ctx
 
-        return self.visitChildren(ctx)
+        #return self.visitChildren(ctx)
+        return None
 
     # Visit a parse tree produced by echoParser#ElifBlock.
     def visitElifBlock(self, ctx:echoParser.ElifBlockContext):
@@ -141,14 +163,17 @@ class echoVisitorMain(echoVisitor):
 
         if currentScope[-1] not in ['if', 'elif']: raise Exception('ELIF must succeed an IF or ELIF statement!')
 
-        cond = self.visit(ctx.expr())
-        if type(cond) != bool: raise Exception('IF statement must be a condition!')
+        # cond = self.visit(ctx.expr())
+        # if type(cond) != bool: raise Exception('IF statement must be a condition!')
+        if type(ctx.expr()).__name__ != 'CompExprContext': raise Exception('IF statement must be a condition!')
 
         AddScope('elif')
 
+        # GetCurrentScope()[0] = []
         GetCurrentScope()[1] = ctx
 
-        return self.visitChildren(ctx)
+        # return self.visitChildren(ctx)
+        return None
 
 
     # Visit a parse tree produced by echoParser#ElseBlock.
@@ -159,7 +184,10 @@ class echoVisitorMain(echoVisitor):
 
         AddScope('else')
 
-        return self.visitChildren(ctx)
+        # GetCurrentScope()[0] = []
+
+        # return self.visitChildren(ctx)
+        return None
 
     # Visit a parse tree produced by echoParser#EndIfBlock.
     def visitEndIfBlock(self, ctx:echoParser.EndIfBlockContext):
@@ -173,25 +201,34 @@ class echoVisitorMain(echoVisitor):
 
         if currentScope[-1] not in ['if', 'elif', 'else']: raise Exception('ENDIF must  succeed an IF, ELIF or ELSE statement')
 
+        print('Scope is ' + str(currentScope))
+
         try:
             ifscope = GetScopeAt(ifscopeIndex)
 
-            self.ExecuteIfStatement(ifscope, ifscopeIndex)
-            print(currentScope)
-            # hasElif = 'elif' in ifscope
-            # hasElse = 'else' in ifscope
-            #
-            # cond = self.visit(ifscope[1].ctx.expr())
-            #
-            # if(cond):
-            #     if hasElif: del ifscope['elif']
-            #     if hasElse: del ifscope['else']
-            #     currentScope = currentScope[:ifscopeIndex + 1]
-            #
-            #     for ins in GetCurrentScope()[0]:
-            #         self.ExecuteInstruction(ins)
+            ifscope[2] = ifscopeIndex
+
+            print(ifscopeIndex)
+            print(currentScope.index('if'))
+            if ifscopeIndex == currentScope.index('if'): self.ExecuteIfStatement(ifscope, ifscopeIndex)
+            else:
+
+                #TODO Nested if statements aren't correctly functioning with an elif. Fix it
+                previousScope = GetScopeAt(ifscopeIndex-1)
+                previousScope[0].append({'if':ifscope})
+                print(previousScope)
+
+                PopScope()
+
+                print(previousScope)
+                #print("\n".join("{}\t{}".format(k, v) for k, v in previousScope.items()))
+
+                print('Scope is ' + str(currentScope))
+
+                # print(previousIfscope)
+
         except Exception as e:
-            raise
+            print(str(e))
         finally:
             pass
             #print('About to pop scope')
@@ -200,12 +237,60 @@ class echoVisitorMain(echoVisitor):
 
         return self.visitChildren(ctx)
 
+    # ++++++++++++++++++++++++++
+    # ++++---- FOR LOOP ----++++
+    # ++++++++++++++++++++++++++
+
+    def ExecuteForStatement(self, forscope, forscopeIndex):
+        iter = forscope[forscope[1]][1]
+        max = forscope[2]
+        step = forscope[3]
+
+        if (step > 0 and iter < max) or (step < 0 and iter > max):
+            self.ExecuteBlockInstructions(forscope[0])
+
+            AssignVariable(forscope[1], iter + step)
+
+            self.ExecuteForStatement(forscope, forscopeIndex)
+        else:
+            while len(currentScope) != forscopeIndex: PopScope()
+
+    # TODO : Replace NEXT keyword with STEP for next parser compile
+
+    # TODO : Support IF statements inside FOR loops
 
     # Visit a parse tree produced by echoParser#ForBlock.
     def visitForBlock(self, ctx:echoParser.ForBlockContext):
         print('#ForBlock')
-        return self.visitChildren(ctx)
 
+        var = ctx.iter_.VAR().getText()
+        value = self.visit(ctx.iter_.expr())
+        max = self.visit(ctx.max_)
+        type = 'int'
+
+        AddScope('for')
+
+        try:
+            DeclareVariable(type, var, value)
+        except Exception as e:
+            PopScope()
+            raise Exception('Iteration value must be a number!')
+
+        forscope = GetCurrentScope()
+
+        forscope[1] = var
+
+        try:
+            forscope[2] = int(max)
+        except TypeError as e:
+            PopScope()
+            raise Exception('Final value must be a number!')
+
+        # Step
+        forscope[3] = 1
+
+
+        return self.visitChildren(ctx)
 
     # Visit a parse tree produced by echoParser#ForNextBlock.
     def visitForNextBlock(self, ctx:echoParser.ForNextBlockContext):
@@ -216,6 +301,14 @@ class echoVisitorMain(echoVisitor):
     # Visit a parse tree produced by echoParser#EndForBlock.
     def visitEndForBlock(self, ctx:echoParser.EndForBlockContext):
         print('#EndForBlock')
+
+        if currentScope[-1] != 'for': raise Exception('ENDFOR must succeed a FOR statement')
+
+        # Last index of for
+        forscopeIndex = len(currentScope) - currentScope[::-1].index('for') - 1
+
+        self.ExecuteForStatement(GetScopeAt(forscopeIndex), forscopeIndex)
+
         return self.visitChildren(ctx)
 
     # Visit a parse tree produced by echoParser#CompExpr.
@@ -285,7 +378,7 @@ class echoVisitorMain(echoVisitor):
 
     # Visit a parse tree produced by echoParser#decl.
     def visitDecl(self, ctx:echoParser.DeclContext):
-        # print('#Decl')
+        #print('#Decl')
 
         try:
             var = ctx.assign_new().VAR().getText()
