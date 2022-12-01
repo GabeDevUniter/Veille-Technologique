ef14bb607b5fcab51779fc3da35abcd405e7b95d
GabeFlyBoy <1793245@cegepat.qc.ca>
2022-12-01 09:25:04 -0500

    Ajout d'un script contenant des classes pour certaines fonctionnalités de Echo (Actuellement inutilisé, importé dans echoVisitorMain)

---
+++ src/bin/lib/EchoClasses.py
@@ -0,0 +1,14 @@
+
+# Currently unused
+class ScopeExecution():
+    """ScopeExecution est utile pour l'exécution des instructions d'un scope
+    qui ne doit pas tout de suite être exécuté (ex: Un scope IF à l'intérieur d'un
+    autre scope IF)"""
+
+    def __init__(self, scopeMethod, scope, scopeIndex):
+        self.scopeMethod
+        self.scope
+        self.scopeIndex
+
+    def execute():
+        self.scopeMethod(self.scope, self.scopeIndex)
